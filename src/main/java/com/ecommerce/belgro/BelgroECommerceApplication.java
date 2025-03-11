88c5f96f5e27f2e2b1885c92ac877a1efd602cb2
Ermiyas Tesfaye <ermiyas@belgro.com>
2025-03-11 15:14:24 +0300

    commit 1

---
+++ src/main/java/com/ecommerce/belgro/BelgroECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.belgro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelgroECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelgroECommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/belgro/BelgroECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.belgro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelgroECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
